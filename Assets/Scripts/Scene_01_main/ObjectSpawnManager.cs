8181133067f3b98f5852dc36b6ef90b4e06c755f
PPC-16016\komiya <komiya_ktk@msoft.co.jp>
2017-12-14 16:01:11 +0900

    Android端末上で動作するように調整 仮のキューブをマップ上に表示

---
+++ Assets/Scripts/Scene_01_main/ObjectSpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoMap;
+using GoShared;
+
+public class ObjectSpawnManager : MonoBehaviour 
+{
+	[SerializeField]
+	private Coordinates m_coordinates;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (_UpdatePosition());
+	}
+
+	private IEnumerator _UpdatePosition(){
+		while(true){
+			OnOriginSet (m_coordinates);
+			yield return new WaitForSeconds(1.0f);
+		}
+	}
+
+	void OnOriginSet (Coordinates currentLocation)
+	{
+		//Position
+		Vector3 currentPosition = currentLocation.convertCoordinateToVector ();
+		currentPosition.y = transform.position.y;
+
+		transform.position = currentPosition;
+	}
+}
