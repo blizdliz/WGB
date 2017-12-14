a425c521bf4483fe27a66faaedbdc02ed60b46d0
PPC-16016\komiya <komiya_ktk@msoft.co.jp>
2017-12-14 17:56:42 +0900

    xmlパーサー作成中

---
+++ Assets/Scripts/common/XmlParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+
+public class XmlParser : MonoBehaviour
+{
+	void Start(){
+		string fullpath = Application.streamingAssetsPath + "/DummyData/DummyData.xml";
+		string content = File.ReadAllText( fullpath );
+		ParseXml (content);
+	}
+
+	public void ParseXml(string content){
+		XmlDocument xmlDoc = new XmlDocument ();
+		xmlDoc.Load (new StringReader(content));
+
+		XmlNode root = xmlDoc.FirstChild;
+		XmlNodeList areaList = xmlDoc.GetElementsByTagName("Area");
+		Debug.Log("FirstChild " + root.InnerText);   //子ノードをふくむ、すべてのタグのテキストが表示される
+
+		XmlNodeList header = root.FirstChild.ChildNodes; //最初のノード＝'Header'タグの、子ノードのリスト
+		foreach (XmlNode node in areaList){
+			Debug.Log(node.Name + ", " + node.InnerText);   //タグ名と、テキストを表示
+		}
+	}
+}
